3f5b61853f9c71f2280bff44b210342a85e6b5b6
boogalooper <jazzy.tver@gmail.com>
2025-12-07 23:01:27 +0300

    оптимизация кода

--- Face alignment.jsx
+++ Face alignment.jsx
@@ -4,12 +4,16 @@
 </javascriptresource>
 */
 #target photoshop
+/**======================================================================== 
+ * Настройки скрипта
+===========================================================================*/
 const moveMode = true// false - выравнивание центра лиц выключено, true - включено
 const transformMode = true // false - масштабирование выключено, true - включено
 const rotateMode = true // false - поворот головы выключен, true - включен
 const angle_ratio = 0.5 // 0-1 - коэффициент применяемый к углу наклона головы
 const global_scale = 0.25 // 0-1 - коэффициент масштабирования для ускорения работы со слоями
 const dialog_mode = DialogModes.NO // DialogModes.ALL - интерактивная траснформация, DialogModes.NO - трансформация без участия пользователя
+/**======================================================================== */
 const ver = 0.13,
     API_HOST = '127.0.0.1',
     API_PORT_SEND = 6310,
@@ -28,24 +32,20 @@ var fd = new faceApi(API_HOST, API_PORT_SEND, API_PORT_LISTEN, new File((new Fil
     str = new Locale();
 $.localize = true
 try {
-    var targetLayers = getSelectedLayersIDs(),
-        len = targetLayers.length;
-    if (len > 1 && fd.exit() && fd.init()) {
-        var selectedLayers = targetLayers.slice(1);
-        app.doForcedProgress("Detect faces", "getFaceBounds(targetLayers)")
+    var targetLayers = getSelectedLayers();
+    if (targetLayers.length > 1 && fd.exit() && fd.init()) {
+        app.doForcedProgress("Detect faces", "getFacePoints(targetLayers)")
         fd.exit()
-        if (targetLayers[0] instanceof Object) {
-            dialog_mode == DialogModes.ALL ? app.activeDocument.suspendHistory("Face alignment", 'transformLayers(targetLayers, targetLayers.shift())') :
-                app.activeDocument.suspendHistory("Face alignment", 'app.doForcedProgress("Align layers", "transformLayers(targetLayers, targetLayers.shift())")');
-        }
+        if (targetLayers[0] instanceof Object)
+            app.activeDocument.suspendHistory("Face alignment", (dialog_mode == DialogModes.ALL ? 'transformLayers(targetLayers, targetLayers.shift())' : 'app.doForcedProgress("Align layers", "transformLayers(targetLayers, targetLayers.shift())")'))
+        else throw new Error(str.errBaseLayer)
     } else { throw new Error(str.errLr) }
 } catch (e) { alert(e, str.err) }
-function getSelectedLayersIDs() {
+function getSelectedLayers() {
     if (!apl.getProperty("numberOfDocuments")) throw new Error(str.errDoc)
     var sel = doc.getProperty("targetLayersIDs"),
-        len = sel.count,
-        output = []
-    for (var i = 0; i < len; i++) {
+        output = [];
+    for (var i = 0; i < sel.count; i++) {
         var id = sel.getReference(i).getIdentifier(),
             kind = lr.getProperty("layerKind", id);
         if (kind == 1 || kind == 5) {
@@ -55,71 +55,71 @@ function getSelectedLayersIDs() {
     }
     return output
 }
-function getFaceBounds(selectedLayers) {
-    app.activeDocument.suspendHistory("Get face bounds", "function blankState () {return}")
-    var len = selectedLayers.length
-    for (var i = 0; i < len; i++) {
-        app.changeProgressText("Get face bounds: " + lr.getProperty("name", selectedLayers[i]))
-        lr.selectLayers([selectedLayers[i]])
+function getFacePoints(lrs) {
+    app.activeDocument.suspendHistory("Detect faces", "function blankState () {return}")
+    for (var i = 0; i < lrs.length; i++) {
+        app.changeProgressText("Get face bounds: " + lr.getProperty("name", lrs[i]))
+        lr.selectLayers([lrs[i]])
         var measurement = {};
-        measurement['bounds'] = lr.descToObject(lr.getProperty("boundsNoEffects", selectedLayers[i]).value);
+        measurement['bounds'] = lr.descToObject(lr.getProperty("boundsNoEffects", lrs[i]).value);
+        app.activeDocument.suspendHistory("Measure Face", "measureFace (measurement)")
+        doc.selectPreviousHistoryState()
+        if (measurement.middle) lrs[i] = new convertToAbsolute(lrs[i], measurement)
+    }
+    function measureFace(o) {
         lr.convertToSmartObject()
         lr.editSmartObject()
-        doc.setScale(global_scale)
-        app.activeDocument.suspendHistory("Measure Face", "measureFace (measurement)")
-        doc.close()
-        doc.selectPreviousHistoryState()
-        if (measurement.middle) selectedLayers[i] = new getAbsoluteBounds(selectedLayers[i], measurement)
-    }
-    function measureFace(measurement) {
         doc.flatten()
         doc.convertToRGB()
-        var f = new File(Folder.temp + '/FD.jpg');
-        doc.saveACopy(f);
-        var result = fd.sendPayload(f.fsName.replace(/\\/g, '\\\\'));
-        if (result && result[263] && result[33] && result[152] && result[127] && result[356]) {
-            measurement['left'] = [result[33][0] * (1 / global_scale), result[33][1] * (1 / global_scale)]
-            measurement['right'] = [result[263][0] * (1 / global_scale), result[263][1] * (1 / global_scale)]
-            measurement['bottom'] = [result[152][0] * (1 / global_scale), result[152][1] * (1 / global_scale)]
-            measurement['faceLeft'] = [result[127][0] * (1 / global_scale), result[127][1] * (1 / global_scale)]
-            measurement['faceRight'] = [result[356][0] * (1 / global_scale), result[356][1] * (1 / global_scale)]
-            measurement['middle'] = findMidpoint(measurement['faceRight'], measurement['faceLeft'])
+        doc.setScale(global_scale)
+        var f = new File(Folder.temp + '/FD.jpg'),
+            k = 1 / global_scale;
+        doc.saveACopy(f)
+        doc.close()
+        var faceMesh = fd.sendPayload(f.fsName.replace(/\\/g, '\\\\'));
+        if (faceMesh && faceMesh[263] && faceMesh[33] && faceMesh[152] && faceMesh[127] && faceMesh[356]) {
+            o['left'] = [faceMesh[33][0] * k, faceMesh[33][1] * k]
+            o['right'] = [faceMesh[263][0] * k, faceMesh[263][1] * k]
+            o['bottom'] = [faceMesh[152][0] * k, faceMesh[152][1] * k]
+            o['faceLeft'] = [faceMesh[127][0] * k, faceMesh[127][1] * k]
+            o['faceRight'] = [faceMesh[356][0] * k, faceMesh[356][1] * k]
+            o['middle'] = getMidpoint(o['faceRight'], o['faceLeft'])
         }
-        function findMidpoint(a, b) { return [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2]; }
+        function getMidpoint(a, b) { return [(a[0] + b[0]) / 2, (a[1] + b[1]) / 2]; }
     }
-    function getAbsoluteBounds(id, measurement) {
+    function convertToAbsolute(id, points) {
         this.id = id
-        this.angle = Math.atan2(measurement.right[1] - measurement.left[1], measurement.right[0] - measurement.left[0]) * 180 / Math.PI
-        this.widthLeft = Math.sqrt(Math.pow(measurement.faceLeft[0] - measurement.left[0], 2) + Math.pow(measurement.faceLeft[1] - measurement.left[1], 2))
-        this.widthRight = Math.sqrt(Math.pow(measurement.faceRight[0] - measurement.right[0], 2) + Math.pow(measurement.faceRight[1] - measurement.right[1], 2))
-        this.width = Math.sqrt(Math.pow(measurement.right[0] - measurement.left[0], 2) + Math.pow(measurement.right[1] - measurement.left[1], 2))
-        this.height = Math.sqrt(Math.pow(measurement.bottom[0] - measurement.middle[0], 2) + Math.pow(measurement.bottom[1] - measurement.middle[1], 2))
-        this.measurement = measurement
-        measurement.middle[0] += measurement.bounds.left
-        measurement.middle[1] += measurement.bounds.top
+        this.angle = Math.atan2(points.right[1] - points.left[1], points.right[0] - points.left[0]) * 180 / Math.PI
+        this.widthLeft = Math.sqrt(Math.pow(points.faceLeft[0] - points.left[0], 2) + Math.pow(points.faceLeft[1] - points.left[1], 2))
+        this.widthRight = Math.sqrt(Math.pow(points.faceRight[0] - points.right[0], 2) + Math.pow(points.faceRight[1] - points.right[1], 2))
+        this.width = Math.sqrt(Math.pow(points.right[0] - points.left[0], 2) + Math.pow(points.right[1] - points.left[1], 2))
+        this.height = Math.sqrt(Math.pow(points.bottom[0] - points.middle[0], 2) + Math.pow(points.bottom[1] - points.middle[1], 2))
+        this.measurement = points
+        points.middle[0] += points.bounds.left
+        points.middle[1] += points.bounds.top
         return
     }
 }
 function transformLayers(targetLayers, baseLayer) {
-    var len = targetLayers.length
+    var len = targetLayers.length,
+        tmp = [];
     lr.selectNoLayers();
     for (var i = 0; i < len; i++) {
         app.changeProgressText("Align layer: " + lr.getProperty("name", targetLayers[i].id))
         if (targetLayers[i] instanceof Object) {
+            tmp.push(targetLayers[i].id)
             doc.selectLayers([targetLayers[i].id])
             app.updateProgress(i + 1, len)
-            if (targetLayers[i] instanceof Object) {
-                var dX = moveMode ? baseLayer.measurement.middle[0] - targetLayers[i].measurement.middle[0] : 0,
-                    dY = moveMode ? baseLayer.measurement.middle[1] - targetLayers[i].measurement.middle[1] : 0,
-                    dW = 100 * (baseLayer.width / targetLayers[i].width),
-                    dH = 100 * (baseLayer.height / targetLayers[i].height),
-                    scale = dH > dW ? dH : dW;
-                lr.move(dX, dY);
-                lr.transform(transformMode ? scale : 100, targetLayers[i].measurement.middle[0]+dX, targetLayers[i].measurement.middle[1]+dY, rotateMode ? -targetLayers[i].angle * angle_ratio : 0, dialog_mode)
-            }
+            var dX = moveMode ? baseLayer.measurement.middle[0] - targetLayers[i].measurement.middle[0] : 0,
+                dY = moveMode ? baseLayer.measurement.middle[1] - targetLayers[i].measurement.middle[1] : 0,
+                dW = 100 * (baseLayer.width / targetLayers[i].width),
+                dH = 100 * (baseLayer.height / targetLayers[i].height),
+                scale = dH > dW ? dH : dW;
+            lr.move(dX, dY);
+            lr.transform(transformMode ? scale : 100, targetLayers[i].measurement.middle[0] + dX, targetLayers[i].measurement.middle[1] + dY, rotateMode ? -targetLayers[i].angle * angle_ratio : 0, dialog_mode)
         }
     }
-    doc.selectLayers(selectedLayers)
+    doc.selectLayers(tmp)
 }
 function AM(target, order) {
     var s2t = stringIDToTypeID,
@@ -132,7 +132,7 @@ function AM(target, order) {
         (r = new AR).putProperty(s2t('property'), property);
         id != undefined ? (idxMode ? r.putIndex(target, id) : r.putIdentifier(target, id)) :
             r.putEnumerated(target, s2t('ordinal'), order ? s2t(order) : s2t('targetEnum'));
-        return descMode ? executeActionGet(r) : getDescValue(executeActionGet(r), property);
+        try { return descMode ? executeActionGet(r) : getDescValue(executeActionGet(r), property) } catch (e) { return false };
     }
     this.hasProperty = function (property, id, idxMode) {
         property = s2t(property);
@@ -140,14 +140,6 @@ function AM(target, order) {
         id ? (idxMode ? r.putIndex(target, id) : r.putIdentifier(target, id))
             : r.putEnumerated(target, s2t('ordinal'), s2t('targetEnum'));
         try { return executeActionGet(r).hasKey(property) } catch (e) { return false }
-    }
-    this.setProperty = function (property, desc) {
-        property = s2t(property);
-        (r = new AR).putProperty(s2t('property'), property);
-        r.putEnumerated(target, s2t('ordinal'), s2t('targetEnum'));
-        (d = new ActionDescriptor).putReference(s2t('null'), r);
-        d.putObject(s2t('to'), property, desc);
-        executeAction(s2t('set'), d, DialogModes.NO);
     }
     this.descToObject = function (d) {
         var o = {}
@@ -171,70 +163,60 @@ function AM(target, order) {
         d.putBoolean(s2t('copy'), true);
         executeAction(s2t('save'), d, DialogModes.NO);
     }
-    this.selectLayers = function (IDList) {
-        var r = new ActionReference()
-        for (var i = 0; i < IDList.length; i++) {
-            r.putIdentifier(s2t("layer"), IDList[i])
-        }
-        (d = new ActionDescriptor()).putReference(s2t("target"), r)
+    this.selectLayers = function (ids) {
+        var r = new AR;
+        for (var a in ids) r.putIdentifier(s2t("layer"), ids[a]);
+        (d = new AD).putReference(s2t("target"), r)
         d.putBoolean(s2t("makeVisible"), true)
         executeAction(s2t("select"), d, DialogModes.NO)
     }
     this.selectNoLayers = function () {
-        (r = new ActionReference()).putEnumerated(s2t("layer"), s2t('ordinal'), s2t('targetEnum'));
-        (d = new ActionDescriptor()).putReference(s2t('target'), r);
+        (r = new AR).putEnumerated(s2t("layer"), s2t('ordinal'), s2t('targetEnum'));
+        (d = new AD).putReference(s2t('target'), r);
         executeAction(s2t('selectNoLayers'), d, DialogModes.NO);
-    }
-    this.isPixlelsLocked = function (desc) {
-        if (desc.getBoolean(desc.getKey(1)) || desc.getBoolean(desc.getKey(2)) || desc.getBoolean(desc.getKey(4))) return true
-        return false
     }
     this.flatten = function () {
         executeAction(s2t('flattenImage'), undefined, DialogModes.NO);
     }
     this.convertToRGB = function () {
-        (d = new ActionDescriptor()).putClass(s2t('to'), s2t('RGBColorMode'))
+        (d = new AD).putClass(s2t('to'), s2t('RGBColorMode'))
         executeAction(s2t('convertMode'), d, DialogModes.NO);
     }
     this.close = function (save) {
-        save = save != true ? s2t("no") : s2t("yes")
-        var desc = new ActionDescriptor()
-        desc.putEnumerated(s2t("saving"), s2t("yesNo"), save)
-        executeAction(s2t("close"), desc, DialogModes.NO)
+        save = save != true ? s2t("no") : s2t("yes");
+        (d = new AD).putEnumerated(s2t("saving"), s2t("yesNo"), save);
+        executeAction(s2t("close"), d, DialogModes.NO);
     }
     this.selectPreviousHistoryState = function () {
-        (r = new ActionReference()).putEnumerated(s2t("historyState"), s2t("ordinal"), s2t("previous"));
-        (d = new ActionDescriptor()).putReference(s2t("null"), r);
+        (r = new AR).putEnumerated(s2t("historyState"), s2t("ordinal"), s2t("previous"));
+        (d = new AD).putReference(s2t("null"), r);
         executeAction(s2t("select"), d, DialogModes.NO);
     }
     this.setScale = function (width) {
-        (d = new ActionDescriptor()).putUnitDouble(s2t("width"), s2t("percentUnit"), width * 100);
+        (d = new AD).putUnitDouble(s2t("width"), s2t("percentUnit"), width * 100);
         d.putBoolean(s2t("scaleStyles"), true);
         d.putBoolean(s2t("constrainProportions"), true);
         d.putEnumerated(s2t("interpolation"), s2t("interpolationType"), s2t("bilinear"));
         executeAction(s2t("imageSize"), d, DialogModes.NO);
     }
     this.transform = function (scale, cX, cY, angle, dialogMode) {
-        dialogMode == DialogModes.ALL ? DialogModes.ALL : DialogModes.NO
-        var d = new ActionDescriptor(),
-            d1 = new ActionDescriptor(),
-            r = new ActionReference();
-        r.putEnumerated(s2t('layer'), s2t('ordinal'), s2t('targetEnum'))
-        d.putReference(s2t('null'), r)
-        d.putEnumerated(s2t('freeTransformCenterState'), s2t('quadCenterState'), s2t('QCSIndependent'))
-        d1.putUnitDouble(s2t('horizontal'), s2t('pixelsUnit'), cX)
-        d1.putUnitDouble(s2t('vertical'), s2t('pixelsUnit'), cY)
-        d.putObject(s2t('position'), s2t('paint'), d1)
-        d.putUnitDouble(s2t('width'), s2t('percentUnit'), scale)
-        d.putUnitDouble(s2t('height'), s2t('percentUnit'), scale)
-        d.putUnitDouble(s2t('angle'), s2t('angleUnit'), angle)
-        d.putEnumerated(s2t('interfaceIconFrameDimmed'), s2t('interpolationType'), s2t('bicubic'))
-        executeAction(s2t('transform'), d, dialogMode)
+        dialogMode == DialogModes.ALL ? DialogModes.ALL : DialogModes.NO;
+        (r = new AR).putEnumerated(s2t('layer'), s2t('ordinal'), s2t('targetEnum'));
+        (d = new AD).putReference(s2t('null'), r);
+        d.putEnumerated(s2t('freeTransformCenterState'), s2t('quadCenterState'), s2t('QCSIndependent'));
+        (d1 = new AD).putUnitDouble(s2t('horizontal'), s2t('pixelsUnit'), cX);
+        d1.putUnitDouble(s2t('vertical'), s2t('pixelsUnit'), cY);
+        d.putObject(s2t('position'), s2t('paint'), d1);
+        d.putUnitDouble(s2t('width'), s2t('percentUnit'), scale);
+        d.putUnitDouble(s2t('height'), s2t('percentUnit'), scale);
+        d.putUnitDouble(s2t('angle'), s2t('angleUnit'), angle);
+        d.putEnumerated(s2t('interfaceIconFrameDimmed'), s2t('interpolationType'), s2t('bicubic'));
+        executeAction(s2t('transform'), d, dialogMode);
     }
     this.move = function (dX, dY) {
-        (r = new ActionReference()).putEnumerated(s2t("layer"), s2t("ordinal"), s2t("targetEnum"));
-        (d = new ActionDescriptor()).putReference(s2t("null"), r);
-        (d1 = new ActionDescriptor()).putUnitDouble(s2t("horizontal"), s2t("pixelsUnit"), dX);
+        (r = new AR).putEnumerated(s2t("layer"), s2t("ordinal"), s2t("targetEnum"));
+        (d = new AD).putReference(s2t("null"), r);
+        (d1 = new AD).putUnitDouble(s2t("horizontal"), s2t("pixelsUnit"), dX);
         d1.putUnitDouble(s2t("vertical"), s2t("pixelsUnit"), dY);
         d.putObject(s2t("to"), s2t("offset"), d1);
         executeAction(s2t("move"), d, DialogModes.NO);
@@ -359,8 +341,9 @@ function faceApi(apiHost, portSend, portListen, apiFile) {
 function Locale() {
     this.err = { ru: 'Скрипт остановлен', en: 'Script stopped' }
     this.errDoc = { ru: 'Нет активного документа!', en: 'No active document!' }
-    this.errLr = { ru: '2 и более слоя должны быть выбраны: нижний слой является образцом размера, остальные будут выровнены по образцу. Слои должны быть незаблокированными!', en: 'Two or more layers must be selected: the bottom layer is the size sample, the others will be aligned to the sample. The layers must be unlocked!' }
+    this.errLr = { ru: '2 и более слоя должны быть выбраны: нижний слой является образцом размера лица. Слои должны быть незаблокированными!', en: 'Two or more layers must be selected: the bottom layer is the face size sample. The layers must be unlocked!' }
     this.errModule = { ru: 'Модуль ' + API_FILE + ' не найден! Убедитесь, что он находится в той же папке что и скрипт!', en: 'Module ' + API_FILE + ' not found! Make sure it in the same folder as the script!' }
     this.errConnection = { ru: 'Невозможно установить соединение c ' + API_FILE, en: 'Impossible to establish a connection with ' + API_FILE }
     this.errStarting = { ru: 'Превышено время ожидания ответа инициализации модуля распознавания лиц! Попробуйте еще раз.', en: 'The face recognition module has timed out initializing! Please try again.' }
+    this.errBaseLayer = { ru: 'Лицо не найдено на нижнем слое!', en: 'Face points not found on bottom layer!' }
 }
